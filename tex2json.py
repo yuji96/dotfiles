cf188762f74542e846bfc26b8d9a2b1a6d7096f7
yuji96 <y.yama683@gmail.com>
2021-05-05 15:18:22 +0900

    modify report style

--- tex2json.py
+++ tex2json.py
@@ -6,3 +6,4 @@ import pyperclip
 if sys.argv[1].endswith(".tex"):
     with open(sys.argv[1]) as f:
         pyperclip.copy(json.dumps(f.read().splitlines(), ensure_ascii=False, indent=2))
+        print("copy to clipboard")
