1d6bb59d172b420d96daab116a1c2f4a2ae37f84
yuji96 <y.yama683@gmail.com>
2021-05-06 23:56:03 +0900

    change ext of original file from .tex to .txt

--- tex2json.py
+++ tex2json.py
@@ -3,7 +3,7 @@ import json
 import pyperclip
 
 
-if sys.argv[1].endswith(".tex"):
+if sys.argv[1].endswith(".tex.txt"):
     with open(sys.argv[1]) as f:
         pyperclip.copy(json.dumps(f.read().splitlines(), ensure_ascii=False, indent=2))
         print("copy to clipboard")
