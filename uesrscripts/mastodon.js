ad34a80f09f959673703770e5298376329d9704e
yuji96 <y.yama683@gmail.com>
2022-05-04 13:35:45 +0900

    update mastodon.js

--- uesrscripts/mastodon.js
+++ uesrscripts/mastodon.js
@@ -1,45 +1,71 @@
 // ==UserScript==
 // @name         replace href
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  try to take over the world!
 // @author       You
+// @match        https://mastodon.com/*
 // @grant        none
 // ==/UserScript==
 
+// FIXME: Replace `mastodon` with your host
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const replaceHref = (aTag) => {
+  aTag.removeAttribute("target");
+  const replaced = aTag
+    .getAttribute("href")
+    .replace(
+      /https:\/\/mastodon\.com\/@.+\/(\d+)/,
+      "https://mastodon.com/web/statuses/$1"
+    );
+  aTag.setAttribute("href", replaced);
+  console.log(replaced);
+};
+
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    // 何かしたいこと
+    if (mutation.target.matches(".status__wrapper")) {
+      const aTag = mutation.target.querySelector(
+        "div.status__info > a.status__relative-time"
+      );
+      if (aTag) {
+        replaceHref(aTag);
+      }
+    }
+  });
+});
+
+const observe = () => {
+  const targetNode = document.querySelector(
+    ".columns-area.columns-area--mobile > div[aria-label]"
+  );
+  console.log(targetNode);
+
+  [
+    ...targetNode.querySelectorAll("div.status__info > a.status__relative-time"),
+  ].map(replaceHref);
+  console.log("initial modifying.");
+
+  observer.observe(targetNode, {
+    childList: true,
+    subtree: true,
+  });
+  console.log("observing");
+};
+
 window.addEventListener(
   "load",
-  function () {
-    //   // Your code here...
-    const targetNode = document.querySelector(
-      ".columns-area.columns-area--mobile > div[aria-label]"
-    );
-    // console.log(targetNode);
-    const pattern = /https:\/\/mastodon\.com\/@.+\/(\d+)/;
-
-    const observer = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-        // 何かしたいこと
-        if (mutation.target.matches(".status__wrapper")) {
-          const aTag = mutation.target.querySelector(
-            "div.status__info > a.status__relative-time"
-          );
-          if (aTag) {
-            aTag.removeAttribute("target");
-            const href = aTag.getAttribute("href");
-            aTag.setAttribute(
-              "href",
-              href.replace(pattern, "https://mastodon.com/web/statuses/$1")
-            );
-            console.log("modified");
-          }
-        }
-      });
-    }).observe(targetNode, {
-      childList: true,
-      subtree: true,
-    });
-    console.log("observing");
+  async (event) => {
+    await sleep(750);
+    observe();
+    document.querySelector(".navigation-panel").onclick = async () => {
+      console.log("clicked.");
+      await sleep(750);
+      observe();
+    };
   },
   false
 );
